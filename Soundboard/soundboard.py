4c4d3f5fde980aed27ced77eb42cff014a4ef979
RoboticRice (Desktop) <16727888+RoboticRice@users.noreply.github.com>
2019-05-16 21:14:16 -0700

    Modified some sounds to test Star Wars stuff

--- Soundboard/soundboard.py
+++ Soundboard/soundboard.py
@@ -26,9 +26,9 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #14 = TXD0
 #15 = RXD0
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+##GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+##GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -36,22 +36,29 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 pygame.mixer.init(48000, -16, 1, 1024)
 
 #Set-Up Sound Files
-sndA = pygame.mixer.Sound("Wilhelm Scream.wav")
-sndB = pygame.mixer.Sound("R2/WEEEOOOOWW.wav")
-#sndC = pygame.mixer.Sound("OTHER/Cantina band.wav")
+pygame.mixer.music.load("OTHER/Cantina band.wav")
+fire1 = pygame.mixer.Sound("XWING/XWing fire.wav")
+fire2 = pygame.mixer.Sound("XWING/XWing fire 2.wav")
+fire3 = pygame.mixer.Sound("XWING/XWing fire 3.wav")
 
 #Set-Up Audio Channels (each channel plays audio seperate from the other)
-bChannel = pygame.mixer.Channel(1) #Background Sound
-sChannel = pygame.mixer.Channel(2) #Sound Effects
+#bChannel = pygame.mixer.Channel(1) #Background Sound
+ChannelA = pygame.mixer.Channel(2) #Sound Effects
+#ChannelB = pygame.mixer.Channel(2) #Sound Effects
+#ChannelC = pygame.mixer.Channel(2) #Sound Effects
 
 print "Nothing. I'm all right."
 
+pygame.mixer.music.play(-1)
 while True:
-   try:
-      if (GPIO.input(4)  == False):
-         bChannel.play(sndA)
-      if (GPIO.input(17) == False):
-         sChannel.play(sndB)
-      sleep(.01)
-   except KeyboardInterrupt:
-      exit()
+    try:
+        if (GPIO.input(4)  == False):
+            ChannelA.play(fire1)
+        if (GPIO.input(17) == False):
+            ChannelA.play(fire2)
+        if (GPIO.input(18) == False):
+            ChannelA.play(fire3)
+        sleep(.01)
+    except KeyboardInterrupt:
+    	pygame.mixer.music.stop()
+        exit()
